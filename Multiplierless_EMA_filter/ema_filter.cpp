8c4c2e945414f9665777a809b463cdd346850f79
Samuele Cornell <cornellsamuele@gmail.com>
2016-10-23 12:25:23 +0200

    Add files via upload

---
+++ Multiplierless_EMA_filter/ema_filter.cpp
@@ -0,0 +1,25 @@
+#include "ema_filter.h"
+
+// constructor
+simple_EMA_bitshift::simple_EMA_bitshift( int Mvalue, int init_val) { 
+
+M= Mvalue; // assign value for M 
+output=init_val; // seed initially 
+
+
+}
+
+
+int simple_EMA_bitshift::filter(int CurrentInput){
+
+ input=CurrentInput; 
+ output+= (input-output) >> M;
+ return output; 
+}
+
+//optional seed function 
+void simple_EMA_bitshift::seed(int init_val){ 
+
+  output=init_val; 
+}
+
---
+++ Multiplierless_EMA_filter/ema_filter.h
@@ -0,0 +1,15 @@
+
+class simple_EMA_bitshift{ 
+public: 
+// input output filter block 
+
+int input;
+int output; 
+int M; // alpha = 1/(2^M) M defines alpha
+
+simple_EMA_bitshift( int Mvalue, int init_value) ; 
+int filter(int CurrentInput);
+void seed( int init_val); 
+  
+};
+
